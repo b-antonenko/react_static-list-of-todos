d8479a5d3590995d15dfa3921524eae969efbb54
Antonenko Bohdan <bohdan1997@gmail.com>
2019-06-10 13:09:54 +0300

    Updates

---
+++ precache-manifest.ae395dd7d4e22bc6b78bd082a1aa6035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f30223ba935137d64540fe87a0c810",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "d00f84c418f640d6028c",
+    "url": "/react_static-list-of-todos/static/css/main.c7c460de.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d00f84c418f640d6028c",
+    "url": "/react_static-list-of-todos/static/js/main.fc15cced.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
